f0e864dd1d124a7a2fac697c24ff504be611ff2b
Boniface Waithaka Thairu <BTHAIRU@safaricom.co.ke>
2025-03-03 14:08:44 +0300

    fixed yarn lint

--- app/dashboard/dashboard.tsx
+++ app/dashboard/dashboard.tsx
@@ -2,7 +2,7 @@ import { Box } from "@mui/material";
 import React from "react";
 import Sidebar from "./sidebar";
 
-export default function Dashboard({ children }) {
+export default function Dashboard({ children }: any) {
   return (
     <Box sx={{ display: "flex" }}>
       <Sidebar />
